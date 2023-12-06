2e56954870a52d1cfe093e185aac91e82805b9ef
srikanth <srikanth123poola@gmail.com>
2023-12-06 09:05:13 +0530

    firstcommit

--- newapp/models.py
+++ newapp/models.py
@@ -7,12 +7,18 @@ class Books(models.Model):
     genre = models.CharField(max_length=100)
     ISBN = models.IntegerField()
     availability_status = models.BooleanField(default=True)
+    def __str__(self):
+        return self.title
 class Library_patron(models.Model):
     name = models.CharField(max_length=100)
     contact_info = models.IntegerField()
     membership_details = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
 class Book_Borrowing(models.Model):
     book = models.CharField(max_length=100)
     patron = models.CharField(max_length=100)
     borrowing_date = models.DateField()
     return_date = models.DateTimeField()
+    def __str__(self):
+        return self.book
